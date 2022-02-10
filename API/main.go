dfb4c941e24a28b1daf359263726aed3ddd9f2d0
Bagas Raditya Nafi <89977556+bagasrna@users.noreply.github.com>
2022-02-10 22:52:32 +0700

    Add files via upload

---
+++ API/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"api/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	v1 := router.Group("/v1")
+
+	v1.GET("/", handler.RootHandler)
+	v1.GET("/hello", handler.HelloHandler)
+	v1.GET("/books/:id/:title", handler.BooksHandler)
+	v1.GET("/query", handler.QueryHandler)
+	v1.POST("/books", handler.PostBookHandler)
+
+	router.Run()
+}
